f9574133275b16fb9fd82b22c41f3a66f2d5a620
Kunal <krinsahuja.01@gmail.com>
2021-12-18 14:00:16 +0530

    modified logout redirection

--- controllers/users.js
+++ controllers/users.js
@@ -37,5 +37,6 @@ module.exports.login=(req,res)=>{
 module.exports.logout=(req,res)=>{
     req.logout();
     req.flash('success','goodbye!')
-    res.redirect('/')
+    const redirectUrl=req.session.returnTo || '/campgrounds'
+    res.redirect(redirectUrl)
 }
