78747aef08c59c7ba8bc703820a654e027d5e9a1
Kunal <krinsahuja.01@gmail.com>
2021-12-19 11:30:31 +0530

    fixed map marker on show page

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,4 +1,5 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoia3VuYWxhaHVqYSIsImEiOiJja3RlaWM0dGgwemhiMnFyd2IzNnhzMnNiIn0.CiWMQyk0mh9mMePYQ7Dx_g';
+
 const map = new mapboxgl.Map({
 container: 'map', // container ID
 style: 'mapbox://styles/mapbox/streets-v11', // style URL
@@ -6,12 +7,12 @@ center: campground.geometry.coordinates, // starting position [lng, lat]
 zoom: 10 // starting zoom
 });
 
-new mapboxgl.marker()
+ new mapboxgl.Marker()
     .setLngLat(campground.geometry.coordinates)
-    .Popup(
-        new mapboxgl.Popup({offfset:25})
-        .setHTML(
-            `<h3>${campground.title}</h3>`
-        )
+    .setPopup(
+        new mapboxgl.Popup({offset:25})
+            .setHTML(
+                `<h3>${campground.title}</h3><p>${campground.location}</p>`
+            )
     )
-    .addTo(map)
+    .addTo(map);
