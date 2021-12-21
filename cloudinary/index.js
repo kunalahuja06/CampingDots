20ca74ffe992d24f1242640e546e46287cab182e
kunalahuja06 <imkunalahuja@gmail.com>
2021-12-21 10:57:52 +0530

    fixed edit page

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -11,7 +11,7 @@ const storage=new CloudinaryStorage({
     cloudinary,
     params:{
         folder:'yelpCamp',
-        allowedFormats:['jpeg','png','jpg']
+        allowedFormats:['jpeg','png','jpg','mp4']
     }
 })
 
