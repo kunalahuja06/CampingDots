6e780c5a504ba194b0145f0ea82b41da1fba0d56
kunalahuja06 <imkunalahuja@gmail.com>
2021-12-22 10:06:33 +0530

    fixed error on app.js

--- app.js
+++ app.js
@@ -121,7 +121,7 @@ app.all('*',(req,res,next)=>{
     next(new ExpressError('Page Not Found',404))
 })
 
-app.useFindAndModify('/campgrounds/mycampgrounds',(req,res,next)=>{
+app.use('/campgrounds/mycampgrounds',(req,res,next)=>{
     next(new ExpressError('login to view mycampgrounds',404))
 })
 
